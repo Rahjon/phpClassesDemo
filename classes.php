22e142ddf7263abd123c9c9c8a27bbce2f11200c
Rahjon Lang <rahjon.lang@f1006-26-34158.bmcc.cuny.edu>
2015-10-27 21:39:13 -0400

    rahjon's class added

--- classes.php
+++ classes.php
@@ -13,12 +13,20 @@
 			public $name;
 			public $age;
 			public $weight;
+			private $breed;
 
 			// the object's constructor
 			public function __construct($_name, $_age, $_weight){
 				$this->name   = $_name;
 				$this->age    = $_age;
 				$this->weight = $_weight;
+
+				if ($this->weight > 25){
+					$this->breed = "Moutain Lion";
+
+
+				}
+
 			}
 
 			public function feedCat($_food){
@@ -40,15 +48,56 @@
 		}
 
 
-		$cat1 = new Cat("Little Buddy", 2, 8);
+
+        $cat1 = new Cat("Little Buddy", 2, 8);
 		$cat2 = new Cat("Sneaky Pete", 0, 2);
+		$cat3 = new Cat("whiskers",1,2);
+
 
 		$cat1->howOld();
 		$cat1->feedCat(6);
 
+		$cat3->howOld();
+
 		echo $cat2->name;
 		echo "<br>";
-	?>
+
+		echo "TESTING!!!!";
+
+		$student1= new Student('Harry', 'Potter', 17);
+		$student2= new Student('Charlie','Brown',16);
+		$student3= new Student('Gary', 'Coleman', 15);
+
+		echo $student1->firstName;
+		echo "<br>";
+		echo $student1->lastName;
+		echo "<br>";
+		echo $student1->age;
+		echo "<br>";
+	
+
+
+//Rahjon's class
+class Student{
+
+	public $firstName;
+	public $lastName;
+	public $age;
+
+	public function __construct($_firstName, $_lastName, $_age){
+
+		$this->firstName = $_firstName;
+		$this->lastName  = $_lastName;
+		$this->age       = $_age;
+	}
+
+
+}
+
+
+
+
+?>
 
 </body>
 </html>
